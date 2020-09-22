7ccb41a79f8a9f643fd394d7d0849dc01046c13a
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-22 19:29:02 +0000

    tidied spinner 2

--- spinner2.js
+++ spinner2.js
@@ -1,19 +1,10 @@
 process.stdout.write('hello from spinner1.js... \rheyyy\n')
 for( let time = 0; time < 10000; time += 1000) {
-setTimeout(() => {
-  process.stdout.write('\r|  ');
+setTimeout(() => {process.stdout.write('\r|  ');
 }, time);
-
-setTimeout(()=>{
-  process.stdout.write('\r/  ');
+setTimeout(()=>{ process.stdout.write('\r/  ');
 }, time + 250);
-
-setTimeout(() =>{
-  process.stdout.write('\r-  ')
+setTimeout(() =>{process.stdout.write('\r-  ')
 }, time + 500);
-
-setTimeout(() => {
-  process.stdout.write('\r\\   ')
-},time + 750);
-
-}
+setTimeout(() => {process.stdout.write('\r\\   ')
+},time + 750);}
