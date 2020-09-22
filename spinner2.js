9e1aa6bde6d1322c897125edca4891a8f66782db
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-22 19:27:38 +0000

    added logic to spinner 2

--- spinner2.js
+++ spinner2.js
@@ -1,33 +1,19 @@
 process.stdout.write('hello from spinner1.js... \rheyyy\n')
-
+for( let time = 0; time < 10000; time += 1000) {
 setTimeout(() => {
   process.stdout.write('\r|  ');
-}, 100);
+}, time);
 
 setTimeout(()=>{
   process.stdout.write('\r/  ');
-}, 300);
+}, time + 250);
 
 setTimeout(() =>{
   process.stdout.write('\r-  ')
-}, 500);
+}, time + 500);
 
 setTimeout(() => {
   process.stdout.write('\r\\   ')
-},700);
+},time + 750);
 
-setTimeout(() => {
-  process.stdout.write('\r|  ');
-}, 900);
-
-setTimeout(()=>{
-  process.stdout.write('\r/  ');
-}, 1100);
-
-setTimeout(() =>{
-  process.stdout.write('\r-  ')
-},1300);
-
-setTimeout(() => {
-  process.stdout.write('\r\\   ')
-},1500);
+}
